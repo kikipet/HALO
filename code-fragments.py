82ffbdad7e8c65263e400bdf61d98771dd21010d
kikipet <songkimwl@gmail.com>
2019-06-28 23:03:41 -0400

    Created code-fragments.py

---
+++ code-fragments.py
@@ -0,0 +1,14 @@
+import time
+
+# time.ctime() gives something like "Sat Jun 29 02:27:50 2019"
+
+# what will be sent to the log
+# "{} {} {} [...]".format(time.ctime(), temp, alt, [...])... well the result of this
+
+# do the sensors write up data into separate files? if so then i'll have to read in a bunch of files :|
+
+# honestly a lot of this is going to depend on exactly what hardware we're using
+
+while True:
+	# code?
+	# better condition for while loop?
