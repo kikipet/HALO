3f80b8c8480e934977803031c1d723851d31fb7d
kikipet <37128373+kikipet@users.noreply.github.com>
2019-07-07 14:13:13 -0400

    Update code-fragments.py

--- code-fragments.py
+++ code-fragments.py
@@ -1,39 +1,61 @@
-#!/bin/python3
-
-import time, math, picamera
-
-# time.ctime() gives something like "Sat Jun 29 02:27:50 2019"
-
-# what will be sent to the log
-# "{} {} {} [...]".format(time.ctime(), temp, alt, [...])... well the result of this
-
-# do the sensors write up data into separate files? if so then i'll have to read in a bunch of files :|
-
-# honestly a lot of this is going to depend on exactly what hardware we're using
-
-# account for possibly not receiving data from a sensor - just repeat the last entry or say N/A or something like that?
-
-alt = lambda p: (10**(math.log10(p/1013.25) + 6) - 1) / 6.8755856 * -1 # where p is in hPa
-
-while True:
-	# code?
-	# better condition for while loop?
-	# read data from their respective sensors
-	# write data to some file
-	time.sleep(30) # not actually sure how often I should collect data
-
-# temperature sensor outputs data in w1_slave file - does it keep all data or just the last recorded data?
-# assuming the second:
-tempData = open("w1_slave", "r") # add path to file name
-lines = tempData.readLines()
-while lines[0].strip()[-3:] != "YES":
-	lines = tempData.readLines()
-temp = float(lines[1][lines[1].find("t=")+2:])/1000 # Celsius
-
-# camera
+import time, math, smbus, RPi.GPIO as GPIO, board, busio, adafruit_mprls, adafruit_mma8451, picamera
 
 cam = picamera.PiCamera()
+
+path = "/sys/bus/w1/devices" # ?
+tempData = open(path+"w1_slave", "r") # add path to the file name
+
+# i2c = busio.I2C(board.SCL, board.SDA)
+mpr = adafruit_mprls.MPRLS(i2c, psi_min=0, psi_max=25)
+mma = adafruit_mma8451.MMA8451(i2c)
+	
+def read_temp():
+	'''read_temp() -> float
+	Reads data from thermometer
+	Returns temperature (Celsius)'''
+	lines = tempData.readLines()
+	while lines[0].strip()[-3:] != "YES":
+		lines = tempData.readLines()
+	return float(lines[1][lines[1].find("t=")+2:])/1000
+
+def read_baro():
+	'''read_barometer() -> tuple
+	Reads data from barometer
+	Returns pressure (hPa), altitude (feet)'''
+	# maybe return in more "standard" unit of pressure
+	p = mpr.pressure # this is float right?
+	return (p, (10**(math.log10(p/1013.25) + 6) - 1) / 6.8755856 * -1)
+
+def read_accel():
+	'''read_accel() -> tuple
+	Reads data from accelerometer
+	Returns acceleration in x, y, z directions'''
+	x, y, z = mma.acceleration
+	return (x, y, z)
+
+def read_co2():
+	'''read_co2() -> float (int?)
+	Reads data from CO2 sensor
+	Returns concentration of CO2 (ppm)'''
+	# this should help: https://github.com/alpacagh/MHZ14-CO2-Logger/blob/master/CO2Reader.py
+	pass
+
+# 1 big log file
+log = open("sensor-data-log", "w")
+
+c = 0
+
 while True:
-    time.sleep(60)
-    cam.capture("IMG_" + time.strftime("%H%M%S", time.localtime()) + ".jpg") # localtime or gmtime?
-    # hm change capture rate based on altitude? (like at a certain point the view each minute will not change all too much)
+	# bad condition for while loop - it should stop at *some* point
+	temp = read_temp()
+	baro = read_baro()
+	accel = read_accel()
+	co2 = read_co2()
+	log.write("{}    {:6.3f} \u00b0C    {:4.3f} hPa  {:6.3f} ft    X: {:.3f} m/s\u00b2  Y: {:.3f} m/s\u00b2  Z: {:.3f} m/s\u00b2    {} ppm".format\
+		  (time.strftime("%H:%M:%S", time.gmtime(time.time()-14400))), temp, baro[0], baro[1], accel[0], accel[1], accel[2], co2))
+	if baro[1] < 30000 or (baro[1] >= 30000 and c == 0):
+		# hm change capture rate based on altitude? (like at a certain point the view each minute will not change all too much)
+		# a bit arbitrary
+		cam.capture("IMG_" + time.strftime("%H%M%S", time.gmtime(time.time()-14400))) + ".jpg") # localtime or gmtime?
+	time.sleep(120)
+	c = 1 - c
