b5f9c1bb95569f18096b31ddd7203d6bac6befa8
kikipet <37128373+kikipet@users.noreply.github.com>
2019-07-25 00:04:43 -0400

    Update baro.py

--- baro.py
+++ baro.py
@@ -1,9 +1,9 @@
 import time, math, busio, adafruit_mprls
 
 i2c = busio.I2C(board.SCL, board.SDA)
-mpr = adafruit_mprls.MPRLS(i2c, psi_min=0, psi_max=25, address=0x00) # change address
+mpr = adafruit_mprls.MPRLS(i2c, psi_min=0, psi_max=25)
 p = mpr.pressure
-alt = (10**(math.log10(p/1013.25) + 6) - 1) / 6.8755856 * -1
+alt = (10**(math.log10(p/1013.25) + 6) - 1) / 6.8755856 * -1 # can be fixed easily (in meters)
 
 while True:
     print ((p, alt))
